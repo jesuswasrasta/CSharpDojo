26b382c555179f887a4ebf5eb7e811aae545ad06
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2013-12-01 16:05:53 +0100

    adding files

---
+++ KataRomanNumerals/KataRomanNumerals.Tests.Unit/CalculatorTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+
+namespace KataRomanNumerals.Tests.Unit
+{
+	[TestFixture]
+	public class CalculatorTests
+    {
+		[Test]
+		public void Calculator_GetRoman_0_Returns_0()
+		{
+			var calculator = new Calculator();
+			Assert.IsTrue(calculator.GetRoman(0) == "0");
+		}
+
+		[Test]
+		public void Calculator_GetRoman_1_Returns_I()
+		{
+			var calculator = new Calculator();
+			Assert.IsTrue(calculator.GetRoman(1) == "I");
+		}
+
+		[Test]
+		public void Calculator_GetRoman_2_Returns_II()
+		{
+			var calculator = new Calculator();
+			Assert.IsTrue(calculator.GetRoman(2) == "II");
+		}
+
+		[Test]
+		public void Calculator_GetRoman_3_Returns_III()
+		{
+			var calculator = new Calculator();
+			Assert.IsTrue(calculator.GetRoman(3) == "III");
+		}
+
+		[Test]
+		public void Calculator_GetRoman_4_Returns_IV()
+		{
+			var calculator = new Calculator();
+			Assert.IsTrue(calculator.GetRoman(4) == "IV");
+		}
+
+		[Test]
+		public void Calculator_GetRoman_5_Returns_V()
+		{
+			var calculator = new Calculator();
+			Assert.IsTrue(calculator.GetRoman(5) == "V");
+		}
+
+		[Test]
+		public void Calculator_GetRoman_6_Returns_VI()
+		{
+			var calculator = new Calculator();
+			Assert.IsTrue(calculator.GetRoman(6) == "VI");
+		}
+	}
+}
---
+++ KataRomanNumerals/KataRomanNumerals/Calculator.cs
@@ -0,0 +1,54 @@
+﻿#region Usings
+using System;
+using System.Collections.Generic;
+
+
+#endregion
+
+
+namespace KataRomanNumerals
+{
+	public class Calculator
+	{
+		public string GetRoman(int number)
+		{
+			if (number == 0)
+			{
+				return "0";
+			}
+
+			var result = string.Empty;
+			if (number > 4)
+			{
+				result = "V";
+			}
+			else if (number > 3)
+			{
+				result = "IV";
+			}
+			else
+			{
+				result += GetRomanFrom1to3(number);
+			}
+
+			return result;
+		}
+
+
+		#region Private Methods
+		private string GetRomanFrom1to3(int number)
+		{
+			if (number > 3)
+			{
+				throw new ArgumentOutOfRangeException("number", "Number have to be 1 < 3");
+			}
+			string result = string.Empty;
+			for (int i = 0; i < number; i++)
+			{
+				result += "I";
+			}
+			return result;
+		}
+		#endregion
+	}
+}
