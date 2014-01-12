3f1f66055d7b3dd35733f364d7287e98808aaf9e
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2014-01-12 19:21:50 +0100

    Using a Test case

--- KataRomanNumerals/KataRomanNumerals.Tests.Unit/CalculatorTests.cs
+++ KataRomanNumerals/KataRomanNumerals.Tests.Unit/CalculatorTests.cs
@@ -11,74 +11,23 @@ namespace KataRomanNumerals.Tests.Unit
 	[TestFixture]
 	public class CalculatorTests
     {
-		[Test]
-		public void Calculator_GetRoman_0_Returns_0()
+		[TestCase(0, Result = "0")]
+		[TestCase(1, Result = "I")]
+		[TestCase(2, Result = "II")]
+		[TestCase(3, Result = "III")]
+		[TestCase(4, Result = "IV")]
+		[TestCase(5, Result = "V")]
+		[TestCase(6, Result = "VI")]
+		[TestCase(7, Result = "VII")]
+		[TestCase(8, Result = "VIII")]
+		[TestCase(9, Result = "IX")]
+		[TestCase(10, Result = "X")]
+		[TestCase(11, Result = "XI")]
+		[TestCase(12, Result = "XII")]
+		public string Calculator_GetRoman(int number)
 		{
 			var calculator = new Calculator();
-			Assert.IsTrue(calculator.GetRoman(0) == "0");
-		}
-
-		[Test]
-		public void Calculator_GetRoman_1_Returns_I()
-		{
-			var calculator = new Calculator();
-			Assert.IsTrue(calculator.GetRoman(1) == "I");
-		}
-
-		[Test]
-		public void Calculator_GetRoman_2_Returns_II()
-		{
-			var calculator = new Calculator();
-			Assert.IsTrue(calculator.GetRoman(2) == "II");
-		}
-
-		[Test]
-		public void Calculator_GetRoman_3_Returns_III()
-		{
-			var calculator = new Calculator();
-			Assert.IsTrue(calculator.GetRoman(3) == "III");
-		}
-
-		[Test]
-		public void Calculator_GetRoman_4_Returns_IV()
-		{
-			var calculator = new Calculator();
-			Assert.IsTrue(calculator.GetRoman(4) == "IV");
-		}
-
-		[Test]
-		public void Calculator_GetRoman_5_Returns_V()
-		{
-			var calculator = new Calculator();
-			Assert.IsTrue(calculator.GetRoman(5) == "V");
-		}
-
-		[Test]
-		public void Calculator_GetRoman_6_Returns_VI()
-		{
-			var calculator = new Calculator();
-			Assert.IsTrue(calculator.GetRoman(6) == "VI");
-		}
-
-		[Test]
-		public void Calculator_GetRoman_7_Returns_VII()
-		{
-			var calculator = new Calculator();
-			Assert.IsTrue(calculator.GetRoman(7) == "VII");
-		}
-
-		[Test]
-		public void Calculator_GetRoman_8_Returns_VIII()
-		{
-			var calculator = new Calculator();
-			Assert.IsTrue(calculator.GetRoman(8) == "VIII");
-		}
-		
-		[Test]
-		public void Calculator_GetRoman_9_Returns_VII()
-		{
-			var calculator = new Calculator();
-			Assert.IsTrue(calculator.GetRoman(9) == "IX");
+			return calculator.GetRoman(number);
 		}
 	}
 }
