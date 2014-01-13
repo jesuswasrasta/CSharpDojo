c0e0aba24a494fbbe414016c86fc4780f7c22872
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2014-01-13 22:32:42 +0100

    KataMoneyExchange: let's start! :)

---
+++ KataMoneyExchange/KataMoneyExchange.Tests.Unit/MoneyExchangerTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+
+namespace KataMoneyExchange.Tests.Unit
+{
+	[TestFixture]
+	public class MoneyExchangerTests
+	{
+		[SetUp]
+		public void Init()
+		{
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+		}
+
+		[Test]
+		public void MoneyExchanger_GetDenominations_AmountZero_ReturnsNoDenominations()
+		{
+			var moneyExchanger = new MoneyExchanger();
+			var denominations = moneyExchanger.GetDenominations(0);
+
+			Assert.IsTrue(denominations.Count == 0);
+		}
+	}
+}
