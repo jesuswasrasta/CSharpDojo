db3a015097da55208bd7223dd42d6d2489813836
jesuswasrasta <jesuswasrasta@yahoo.it>
2015-02-20 18:01:19 +0100

    Stub of FizzBuzz kata, to implement

---
+++ KataFizzBuzz/KataFizzBuzz.Tests/FizzBuzzerTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+
+namespace KataFizzBuzz.Tests
+{
+	[TestFixture]
+    public class FizzBuzzerTests
+    {
+		[Test]
+		public void FizzBuzzer_3_Returns_Fizz()
+		{
+			const string expected = "Fizz";
+			var fizzBuzzer = new FizzBuzzer();
+			var result = fizzBuzzer.Say(3);
+
+			Assert.AreEqual(expected, result);
+		}
+    }
+}
---
+++ KataFizzBuzz/KataFizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace KataFizzBuzz
+{
+	public class FizzBuzzer
+	{
+		public string Say(int number)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
