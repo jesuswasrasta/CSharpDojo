888f3fbc0c411e6db3d3e30039103d672508af88
Ferdinando Santacroce <ferdinando.santacroce@office.7pixel.it>
2016-03-04 16:00:13 +0100

    KataSmallInt: find the smallest int in array

---
+++ KataSmallestInt/SmallestInt.Tests/IntArrayTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+using SmallestInt;
+
+
+namespace SmallestInts.Tests
+{
+	[TestFixture]
+	public class IntArrayTests
+	{
+		[TestCase(new[] { 78, 56, 232, 12, 11, 43 }, ExpectedResult = 11)]
+		[TestCase(new[] { 78, 56, -2, 12, 8, -33 }, ExpectedResult = -33)]
+		public int FixedTest(int[] args)
+		{
+			var intArray = new IntArray();
+			var result = intArray.FindSmallestInt(args);
+			return result;
+		}
+	}
+}
---
+++ KataSmallestInt/SmallestInt/IntArray.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace SmallestInt
+{
+	public class IntArray
+	{
+		public int FindSmallestInt(int[] args)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
