d582e253d0f079fb7f3036607fe1f0a272335d05
jesuswasrasta <jesus_was_rasta@yahoo.it>
2016-03-13 11:37:17 +0100

    Sorting integers, like I was a child...

--- KataSmallestInt/SmallestInt.Tests/IntArrayTests.cs
+++ KataSmallestInt/SmallestInt.Tests/IntArrayTests.cs
@@ -10,7 +10,7 @@ namespace SmallestInts.Tests
 	{
 		[TestCase(new[] { 78, 56, 232, 12, 11, 43 }, ExpectedResult = 11)]
 		[TestCase(new[] { 78, 56, -2, 12, 8, -33 }, ExpectedResult = -33)]
-		public int FixedTest(int[] args)
+		public int FindSmallestIntTest(int[] args)
 		{
 			var intArray = new IntArray();
 			var result = intArray.FindSmallestInt(args);
--- KataSmallestInt/SmallestInt/IntArray.cs
+++ KataSmallestInt/SmallestInt/IntArray.cs
@@ -7,7 +7,22 @@ namespace SmallestInt
 	{
 		public int FindSmallestInt(int[] args)
 		{
-			throw new NotImplementedException();
+			var finished = false;
+			while (!finished)
+			{
+				finished = true;
+				for (var i = 1; i < args.Length; i++)
+				{
+					if (args[i] < args[i - 1])
+					{
+						var a = args[i - 1];
+						args[i - 1] = args[i];
+						args[i] = a;
+						finished = false;
+					}
+				}
+			}
+			return args[0];
 		}
 	}
 }
