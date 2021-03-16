7f70f7e1cbd24a2975c8a7fff7b560aba51fb96c
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2021-03-16 21:26:15 +0100

    KataSmallestInt: LINQ to the resque

--- KataSmallestInt/KataSmallestInt/IntArray.cs
+++ KataSmallestInt/KataSmallestInt/IntArray.cs
@@ -1,25 +1,15 @@
-﻿namespace KataSmallestInt
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace KataSmallestInt
 {
 	public class IntArray
 	{
 		public int FindSmallestInt(int[] args)
 		{
-			var finished = false;
-			while (!finished)
-			{
-				finished = true;
-				for (var i = 1; i < args.Length; i++)
-				{
-					if (args[i] < args[i - 1])
-					{
-						var a = args[i - 1];
-						args[i - 1] = args[i];
-						args[i] = a;
-						finished = false;
-					}
-				}
-			}
-			return args[0];
-		}
+			var ints = new List<int>(args);
+            ints.Sort();
+            return ints.First();
+        }
 	}
 }
